fcde949e19c929099071748f7c2bb5b460451008
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-10 16:19:52 -0400

    pasando variables a html fail

---
+++ render_vars.js
@@ -0,0 +1,23 @@
+var http= require("http"),
+	fs=require("fs");
+
+
+http.createServer(function (req,res) {
+fs.readFile("./index.html", function (error,html) {
+	var html_string=html.toString();
+
+	var variables=html_string.match(/[^\{\}]+(?=\})/g);
+	var nombre="Pablo";
+
+	for (var i = variables.length - 1; i >= 0; i--) {
+		var value = eval("variables[i]");
+		html_string=html_string.replace("{"+variables[i]+"}", value);
+	}
+
+	res.writeHead(200,{"Content-Type":"text/html"});
+	res.write(html_string);
+	res.end();
+
+	});
+
+}).listen(8080);
