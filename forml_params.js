d6bc65a2bd8971fc817bcbae10c85cdeb1d3e9be
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-11 23:47:37 -0400

    Modularizando con funcion parser

--- forml_params.js
+++ forml_params.js
@@ -1,5 +1,8 @@
 var http= require("http"),
-	fs=require("fs");
+	fs=require("fs"),
+	parser =require("./params_parse.js");
+
+var p = parser.parse;
 
 
 http.createServer(function (req,res) {
@@ -12,24 +15,13 @@ http.createServer(function (req,res) {
 fs.readFile("./index.html", function (error,html) {
 	
 	var html_string=html.toString();
-	var arreglo_parametros= [], parametros = {};
+	
 	var variables=html_string.match(/[^\{\}]+(?=\})/g);
 	var nombre="Pablo";
 
-	if (req.url.indexOf("?")>0) {
-		var url_data = req.url.split("?");
-		var arreglo_parametros = url_data[1].split("&");
-		
-	}
+	var parametros=p(req);
 
-	for (var i = arreglo_parametros.length - 1; i >= 0; i--) {
-		var parametro = arreglo_parametros[i];
-		//nombre=pablo
-		var param_data= parametro.split("=");
-		parametros[param_data[0]]= [param_data[1]];
-		//{nombre:pablo}
-
-	}
+	
 
 	for (var i = variables.length - 1; i >= 0; i--) {
 		var variable = variables[i];
--- params_parse.js
+++ params_parse.js
@@ -1,3 +1,22 @@
 function parse (req) {
-	
+	var arreglo_parametros= [], parametros = {};
+
+	if (req.url.indexOf("?")>0) {
+		var url_data = req.url.split("?");
+		var arreglo_parametros = url_data[1].split("&");
+		
+	}
+
+	for (var i = arreglo_parametros.length - 1; i >= 0; i--) {
+		var parametro = arreglo_parametros[i];
+		//nombre=pablo
+		var param_data= parametro.split("=");
+		parametros[param_data[0]]= [param_data[1]];
+		//{nombre:pablo}
+
+	}
+
+	return parametros;
 }
+
+module.exports.parse=parse;
