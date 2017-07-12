8b725fec01fccdd8a2c3905bdc80704b8b3e372e
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-12 00:38:52 -0400

    Usando Jade coomo render

--- hola_express/app.js
+++ hola_express/app.js
@@ -1,7 +1,10 @@
 var express = require("express");
 
 var app = express();
+
+app.set("view engine", "jade");
+
 app.get("/", function (req, res) {
-	res.send("Hola Mundo");
+	res.render("index",{hola: "Hola Pablooooo"});
 });
 app.listen(8080);
