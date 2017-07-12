b56b5dd4317f107e16aa1b3be0ca117b9748f86d
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-12 18:25:57 -0400

    Hola muundo mejorado con variables, jade y express enviado /nombre

--- routing/app.js
+++ routing/app.js
@@ -4,10 +4,14 @@ var app = express();
 
 app.set("view engine", "jade");
 //Verbos Http => GET / POST /PUT / PATCH / OPTIONS / HEADERS / DELETE
+// REST
 app.get("/", function (req, res) {
 	res.render("index");
 });
-
+app.get("/:nombre", function (req, res) {
+	console.log(req.params.nombre);
+	res.render("form", {nombre:req.params.nombre});
+});
 app.post("/", function (req, res) {
 	res.render("form");
 })
