cec7d4eb24b74017ceca07f8e2924880256187d5
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-12 18:51:21 -0400

    Usando layout con jade login/index

--- proyecto/app.js
+++ proyecto/app.js
@@ -5,6 +5,11 @@ var app = express();
 app.set("view engine", "jade");
 
 app.get("/", function (req, res) {
-	res.render("index",{hola: "Hola Pablooooo"});
+	res.render("index");
 });
+
+app.get("/login", function (req, res) {
+	res.render("login");
+});
+
 app.listen(8080);
