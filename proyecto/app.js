a8f17d70f8bc120cd291538c92ceda36c69286a9
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-12 20:41:47 -0400

    enviando y recibiendo parametros introducidos por el usuario en login con body-parser

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,9 +1,11 @@
 var express = require("express");
-
+var bodyParser=require("body-parser");
 var app = express();
+
 app.use("estatico",express.static("public"));
 app.use("estatico",express.static("assets"));
-
+app.use(bodyParser.json()); //peticiones application/json
+app.use(bodyParser.urlencoded({extended:true})); 
 app.set("view engine", "jade");
 
 app.get("/", function (req, res) {
@@ -13,5 +15,12 @@ app.get("/", function (req, res) {
 app.get("/login", function (req, res) {
 	res.render("login");
 });
+
+app.post("/users", function (req,res) {
+
+	console.log("Contrasena "+req.body.password);
+	console.log("Email "+req.body.email);
+	res.send("Recibimos tus datos");
+})
 
 app.listen(8080);
