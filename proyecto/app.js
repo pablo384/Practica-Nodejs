366e628ee2c305f36e605e19f4468ec6e989b279
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-15 13:58:38 -0400

    COnexion con MongoDB Correcta

--- proyecto/app.js
+++ proyecto/app.js
@@ -4,10 +4,19 @@ var app = express();
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+//mongoAtlas
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://pablo384:2323@pruebas-shard-00-00-uiwkf.mongodb.net:27017,pruebas-shard-00-01-uiwkf.mongodb.net:27017,pruebas-shard-00-02-uiwkf.mongodb.net:27017/pruebas?ssl=true&replicaSet=pruebas-shard-0&authSource=admin";
+
+
+
+
+
 //conexion con bd
-mongoose.createConnection("mongodb://localhost/fotos", {
-	useMongoClient: true,
-});
+// mongoose.createConnection("mongodb://localhost/fotos", {
+// 	useMongoClient: true,
+// });
 
 var userSchemaJSON={
 	email:String,
@@ -29,26 +38,47 @@ app.get("/", function (req, res) {
 });
 
 app.get("/login", function (req, res) {
-	User.find(function (err,doc) {
-		console.log(doc);
-		res.render("login");
-	})
+
+	// var cursor = db.collection('Users').find({});
+	// console.log(cursor);
+	// User.find(function (err,doc) {
+	// 	console.log(doc);
+	res.render("login");
+	// })
 	
 });
 
 app.post("/users", function (req,res) {
-	//creando usuario
-	var user = new User({email:req.body.email, password:req.body.password});
+	
+	MongoClient.connect(uri, function(err, db) {
+	  // Paste the following examples here
 
-	user.save(function (err) {
-		if (err) {console.log(err)}
-		else {
-			res.send("Recibimos tus datos");
-		}
+//--------------------------------------------
+	db.collection('inventory').insertMany([
+	   // MongoDB adds the _id field with an ObjectId if _id is not present
+	   { item: "journal", qty: 25, status: "A",
+	       size: { h: 14, w: 21, uom: "cm" }, tags: [ "blank", "red" ] },
+	   { item: "notebook", qty: 50, status: "A",
+	       size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank" ] },
+	   { item: "paper", qty: 100, status: "D",
+	       size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank", "plain" ] },
+	   { item: "planner", qty: 75, status: "D",
+	       size: { h: 22.85, w: 30, uom: "cm" }, tags: [ "blank", "red" ] },
+	   { item: "postcard", qty: 45, status: "A",
+	       size: { h: 10, w: 15.25, uom: "cm" }, tags: [ "blue" ] }
+	])
+	.then(function(result) {
+	  // process result
+	});
 
-	})
-	// console.log("Contrasena "+req.body.password);
-	// console.log("Email "+req.body.email);
+
+//----------------------------------------------
+
+
+
+
+	  db.close();
+	});
 	
 })
 
