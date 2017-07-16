8e9f66b5141903c4ef24b17de1d6a59db757bf1b
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-07-16 01:29:23 -0400

    Separado login y registro, creada url /sessions

--- proyecto/app.js
+++ proyecto/app.js
@@ -14,31 +14,44 @@ app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.get("/login", function (req, res) {
+app.get("/signup", function (req, res) {
 
     User.find(function(err,doc){
         console.log(doc);
-        res.render("login");
-    })
-
-    
+        res.render("signup");
+    })    
+});
+app.get("/login", function (req, res) {
+    res.render("login");
+       
 });
 
-app.post("/users", function (req,res) {
+app.post("/sessions", function (req,res) {
 
-    var user= new User({
-        email: req.body.email, 
-        password: req.body.password,
-        password_confirmation: req.body.password_confirmation,
-        username:req.body.username
-    });
-
-    user.save().then(function (us) {
-        res.send("Guardamos tus datos exitosamente");
-    }, function (err) {
-        console.log(String(err));
-        res.send("No pude guardar los datos (O.o)");
+    // User.findById("596af11f3190b66240d97205",function (err,doc) {
+        
+    // });
+    User.findOne({
+        email:req.body.email,
+        password: req.body.password},function (err,docs) {
+            console.log(docs);
+            res.send("Hola Mundo");
+        
     })
+
+    // var user= new User({
+    //     email: req.body.email, 
+    //     password: req.body.password,
+    //     password_confirmation: req.body.password_confirmation,
+    //     username:req.body.username
+    // });
+
+    // user.save().then(function (us) {
+    //     res.send("Guardamos tus datos exitosamente");
+    // }, function (err) {
+    //     console.log(String(err));
+    //     res.send("No pude guardar los datos (O.o)");
+    // })
     
 
     // user.save(function (err, user, numero) {
